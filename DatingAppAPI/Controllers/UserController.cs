afc683c297d118b7eb6d0b9217beb531a1ebd333
Dheepak <dheepak.l@lucidimaging.in>
2024-02-05 09:57:07 +0530

    Add .gitignore entries for obj and bin folders Update database connection string Added DataContext to UserController (injected) Added Initial Migrations and Database created for Initial Migrations

--- DatingAppAPI/Controllers/UserController.cs
+++ DatingAppAPI/Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using DatingAppAPI.Data;
+using DatingAppAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingAppAPI.Controllers
@@ -6,14 +8,23 @@ namespace DatingAppAPI.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        public UserController()
+        private readonly DataContext _context;
+        public UserController(DataContext context)
         {
-
+            _context = context;
         }
 
-        public ActionResult<string> GetUsers()
+
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
         {
-            return "This will return a list of users";
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == id);
         }
 
     }
--- DatingAppAPI/Program.cs
+++ DatingAppAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
