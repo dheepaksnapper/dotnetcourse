fc9d1f82b0d1404ddfe84379a0e80ae27be1ee0e
dheepaksnapper <16it10is@gmail.com>
2024-04-02 20:05:09 +0530

    Added a BaseApiController and implemented in USer and WeatherForecast controllers

---
+++ DatingAppAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingAppAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
--- DatingAppAPI/Controllers/UserController.cs
+++ DatingAppAPI/Controllers/UserController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingAppAPI.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UserController : BaseApiController
     {
         private readonly DataContext _context;
         public UserController(DataContext context)
