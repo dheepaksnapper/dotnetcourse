6b0d2fe171404eaa7bb25fe6aa9fab2b9da0b8d6
Dheepak <dheepak.l@lucidimaging.in>
2024-04-02 10:03:37 +0530

    -Add password hash and salt properties to User entity and therie required migrations

--- DatingAppAPI/Data/Migrations/20240402043052_PasswordAdded.Designer.cs
+++ DatingAppAPI/Data/Migrations/20240402043052_PasswordAdded.Designer.cs
@@ -2,6 +2,7 @@
 using DatingAppAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingAppAPI.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240402043052_PasswordAdded")]
+    partial class PasswordAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.14");
