0c97b568ae8b06388b4c00c2fc546c6bc17dd41b
dheepaksnapper <16it10is@gmail.com>
2024-04-16 06:48:59 +0530

    -Add SSL folder to git ignore -Implemented Angular template forms for login (two-way binding)

--- DatingAppClient/src/app/nav/nav.component.ts
+++ DatingAppClient/src/app/nav/nav.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+  model : any = {};
+
+  ngOnInit(): void {
+    console.log('NavComponent initialized.');
+  }
+
+  login(){
+    console.log(this.model);
+  }
 }
